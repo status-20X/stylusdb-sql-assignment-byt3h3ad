c76abab73cd5108a245f1f6b6a281bcd740c7c4f
byt3h3ad <adhirajd007@gmail.com>
2024-04-29 01:22:55 +0530

    20 fixed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ const { executeDELETEQuery } = require("./queryExecutor");
 const { parseINSERTQuery } = require("./queryParser");
 const { readCSV, writeCSV } = require("./csvReader");
 
-const SQL_QUERY = "DELETE FROM courses WHERE course_id = '2'";
-
-executeDELETEQuery(SQL_QUERY).then((data) => {
-	console.log("Query Executed", data);
-});
+module.exports = { executeDELETEQuery, parseINSERTQuery, readCSV, writeCSV };
